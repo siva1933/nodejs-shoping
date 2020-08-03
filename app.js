c1f8cc659704feeeee188d403d051d46c37e4605
siva <b121933rgukt@gmail.com>
2020-08-03 20:56:42 +0530

    rest-api first commit

--- app.js
+++ app.js
@@ -1,119 +1,20 @@
-const path = require('path');
-
 const express = require('express');
-const csrf = require('csurf');
-const flash = require('connect-flash');
-
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
-const mongoose = require('mongoose');
-const multer = require('multer');
-const { uri } = require("./util/db");
-
-const store = new MongoDBStore({
-  uri,
-  collection: "sessions"
-})
 const bodyParser = require('body-parser');
-// const expressHbs = require('express-handlebars');
-const errorController = require("./controllers/error")
 
-const User = require("./models/user")
+const feedRoutes = require('./routes/feed');
 
 const app = express();
 
-const csrfProtection = csrf();
-
-const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'images')
-  },
-  filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + "-" + file.originalname)
-  }
-})
-
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype === "image/png" || file.mimetype === "image/jpg" || file.mimetype === "image/jpeg") {
-    cb(null, true)
-  } else {
-    cb(null, false)
-  }
-}
-
-
-
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const authRoutes = require('./routes/auth');
-
-
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'))
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use("/images", express.static(path.join(__dirname, 'images')));
-app.use(session({
-  secret: "my-secret",
-  resave: false, // session is not saved on every request
-  saveUninitialized: false, // 
-  store
-}))
-
-app.use(csrfProtection)
-app.use(flash())
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
 
 app.use((req, res, next) => {
-  res.locals.csrfToken = req.csrfToken()
-  next()
-})
-
-
-app.use((req, res, next) => {
-  if (!req.session.user) {
-    next()
-  } else {
-    User.findOne({ email: req.session.user.email }).then((user) => {
-      if (!user) {
-        next()
-      }
-      req.user = user
-      next()
-    }).catch((err) => {
-      return next(new Error(err))
-    })
-  }
-
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
 });
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
-app.use(authRoutes);
+app.use('/feed', feedRoutes);
 
-// app.get('/500', errorController.get500)
-
-app.use(errorController.get404);
-
-// Error handling middleware
-
-app.use((error, req, res, next) => {
-  const isLoggedIn = req.session.isLoggedIn
-  console.log(error)
-  res.status(500).render('500', {
-    pageTitle: 'Internal Server Error', path: "/500",
-    isAuthenticated: isLoggedIn
-  });
-
-})
-
-mongoose.connect(uri).then(() => {
-  app.listen(3001)
-}).catch((err) => {
-  console.error(err)
-})
+app.listen(8080);
